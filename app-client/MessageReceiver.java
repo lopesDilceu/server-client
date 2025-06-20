a1068cea505f033d13e18632e07ada06c7f10ef4
thiagorezendev <thiagor3zende@gmail.com>
2025-06-20 20:21:18 -0300

    feat: add client

---
+++ app-client/MessageReceiver.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MessageReceiver implements Runnable {
+    private Socket socket;
+
+    public MessageReceiver(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try (
+                // Objeto para ler as mensagens que o servidor envia
+                BufferedReader leitorServidor = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            String mensagemDoServidor;
+            // Loop para ficar ouvindo o servidor
+            while ((mensagemDoServidor = leitorServidor.readLine()) != null) {
+                // Descriptografa a mensagem recebida
+                String mensagemOriginal = Criptografia.descriptografar(mensagemDoServidor);
+                System.out.println("Recebido: " + mensagemOriginal);
+            }
+        } catch (Exception e) {
+            // Se o servidor cair ou a conexão for fechada, a exceção será capturada
+            System.out.println("Conexão com o servidor perdida.");
+        }
+    }
+}
